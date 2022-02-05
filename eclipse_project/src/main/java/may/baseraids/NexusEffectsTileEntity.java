2e0ce95935bc67c0b158c7a7762e5276b1a54d38
tatschi <natascha.may@outlook.de>
2022-02-05 22:25:14 +0100

    Detailed cleanup NexusEffectsTileEntity, new class RepeatingSoundEffect

--- eclipse_project/src/main/java/may/baseraids/NexusEffectsTileEntity.java
+++ eclipse_project/src/main/java/may/baseraids/NexusEffectsTileEntity.java
@@ -1,50 +1,65 @@
 package may.baseraids;
 
 import java.util.List;
-
+import may.baseraids.sounds.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.AxisAlignedBB;
 
+/**
+ * This class handles sound and effects from the nexus. Effects are not
+ * implemented as of 1.16.4-0.1.
+ * 
+ * @author Natascha May
+ * @since 1.16.4-0.1
+ */
+public class NexusEffectsTileEntity extends TileEntity implements ITickableTileEntity {
 
+	EffectInstance curEffect = null;
+	double effectDistance = 50D;
 
-public class NexusEffectsTileEntity extends TileEntity implements ITickableTileEntity{
+	final static EffectInstance REGEN_EFFECT_AFTER_RAID_WIN = new EffectInstance(Effects.REGENERATION, 100, 0, false,
+			true);
 
-	
-	
-	Effect curEffect = null;
-	
+	final static RepeatingSoundEffect NEXUS_AMBIENT_SOUND = new RepeatingSoundEffect(SoundEvents.BLOCK_BEACON_AMBIENT,
+			SoundCategory.BLOCKS, 2.0F, 0.5F, 80);
+
 	public NexusEffectsTileEntity() {
 		super(Baseraids.NEXUS_TILE_ENTITY_TYPE.get());
 	}
-	
-	
-	// the tick method is called each tick (approx. 20 ticks per second)
+
 	public void tick() {
-		if (this.world.getGameTime() % 80L == 0L) {
-            this.addEffectsToPlayers();
-            this.world.playSound((PlayerEntity)null, this.pos, SoundEvents.BLOCK_BEACON_AMBIENT, SoundCategory.BLOCKS, 2.0F, 0.5F);
-      	}
-		
+		addEffectsToPlayers();
+		if (world.getGameTime() % (NEXUS_AMBIENT_SOUND.intervalInTicks - 5) == 0L) {
+			NEXUS_AMBIENT_SOUND.playSound(world, null, pos);
+		}
 	}
-	
+
+	/**
+	 * Adds the effect <code>curEffect</code> to all players in the distance <code>effectDistance</code>.
+	 */
 	private void addEffectsToPlayers() {
-		if (!this.world.isRemote && curEffect != null) {
-			double distance = (double)(50);
-			int amplifier = 0;
-	
-			int duration = 100;
-			AxisAlignedBB axisalignedbb = (new AxisAlignedBB(this.pos)).grow(distance).expand(0.0D, (double)this.world.getHeight(), 0.0D);
-			List<PlayerEntity> list = this.world.getEntitiesWithinAABB(PlayerEntity.class, axisalignedbb);
-	
-			for(PlayerEntity playerentity : list) {
-				playerentity.addPotionEffect(new EffectInstance(curEffect, duration, amplifier, true, true));
-			}
+		if (world.isRemote) {
+			return;
+		}
+		if (curEffect == null) {
+			return;
+		}
+		if (world.getGameTime() % (curEffect.getDuration() / 2) != 0) {
+			return;
+		}
+
+		AxisAlignedBB axisalignedbb = (new AxisAlignedBB(pos)).grow(effectDistance).expand(0.0D,
+				(double) world.getHeight(), 0.0D);
+
+		List<PlayerEntity> list = world.getEntitiesWithinAABB(PlayerEntity.class, axisalignedbb);
+		for (PlayerEntity playerentity : list) {
+			playerentity.addPotionEffect(curEffect);
 		}
 	}
 
---
+++ eclipse_project/src/main/java/may/baseraids/sounds/RepeatingSoundEffect.java
@@ -0,0 +1,39 @@
+package may.baseraids.sounds;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * This class characterizes a repeating sound effect with it's properties like
+ * volume, pitch and interval between the sound. It can be used to easily define
+ * fixed sound effects.
+ * 
+ * @author Natascha May
+ * @since 1.16.4-0.1.1
+ */
+public class RepeatingSoundEffect {
+
+	SoundEvent soundEvent;
+	SoundCategory soundCategory;
+	float volume;
+	float pitch;
+	public int intervalInTicks;
+
+	public RepeatingSoundEffect(SoundEvent soundEvent, SoundCategory soundCategory, float volume, float pitch,
+			int intervalInTicks) {
+		this.soundEvent = soundEvent;
+		this.soundCategory = soundCategory;
+		this.volume = volume;
+		this.pitch = pitch;
+		this.intervalInTicks = intervalInTicks;
+	}
+
+	public void playSound(World world, @Nullable PlayerEntity player, BlockPos pos) {
+		world.playSound(player, pos, soundEvent, soundCategory, volume, pitch);
+	}
+}
