6f489b83affb383a6dfdd1dc4c431de0cecb5f3c
Natascha May <Natascha.May@fau.de>
2022-06-15 13:57:59 +0200

    Added BaseraidsEntityManager

---
+++ eclipse_project/src/main/java/may/baseraids/entities/BaseraidsEntityManager.java
@@ -0,0 +1,20 @@
+package may.baseraids.entities;
+
+import net.minecraft.entity.monster.SkeletonEntity;
+import net.minecraft.entity.monster.SpiderEntity;
+import net.minecraft.entity.monster.ZombieEntity;
+
+public class BaseraidsEntityManager {
+
+	public void setupZombieGoals(ZombieEntity entity) {
+		
+	}
+	
+	public void setupSpiderGoals(SpiderEntity entity) {
+			
+	}
+	
+	public void setupSkeletonGoals(SkeletonEntity entity) {
+		
+	}
+}
