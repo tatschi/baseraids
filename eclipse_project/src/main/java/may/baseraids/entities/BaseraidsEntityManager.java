6f0e8a520ed5fd06aa5caf69b8bd4e0a0092a974
Natascha May <Natascha.May@fau.de>
2022-06-16 12:57:58 +0200

    Implemented setupSpiderGoals()

--- eclipse_project/src/main/java/may/baseraids/entities/BaseraidsEntityManager.java
+++ eclipse_project/src/main/java/may/baseraids/entities/BaseraidsEntityManager.java
@@ -11,6 +11,8 @@ import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
 import net.minecraft.entity.ai.goal.MoveThroughVillageGoal;
 import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.PhantomEntity;
 import net.minecraft.entity.monster.SkeletonEntity;
 import net.minecraft.entity.monster.SpiderEntity;
 import net.minecraft.entity.monster.ZombieEntity;
@@ -18,23 +20,42 @@ import net.minecraft.entity.monster.ZombieEntity;
 public class BaseraidsEntityManager {
 
 	public void setupZombieGoals(ZombieEntity entity) {
+		// remove unwanted goals
+		final List<Class<? extends Goal>> goalClassesToRemove = Arrays.asList(ZombieEntity.AttackTurtleEggGoal.class,
+				LookRandomlyGoal.class, MoveThroughVillageGoal.class, WaterAvoidingRandomWalkingGoal.class);
+		removeGoalsFromList(entity, goalClassesToRemove);
+		
 		entity.goalSelector.addGoal(0, new DestroyNexusGoal(entity, Baseraids.baseraidsData.raidManager));
 		entity.goalSelector.addGoal(1, new BlockBreakGoal(entity, Baseraids.baseraidsData.raidManager));
 		entity.goalSelector.addGoal(3, new MoveTowardsNexusGoal(entity, Baseraids.baseraidsData.raidManager));
-
-		// remove unwanted goals
-		List<Class<? extends Goal>> goalClassesToRemove = Arrays.asList(ZombieEntity.AttackTurtleEggGoal.class,
-				LookRandomlyGoal.class, MoveThroughVillageGoal.class, WaterAvoidingRandomWalkingGoal.class);
-
-		entity.goalSelector.getRunningGoals().filter((goal) -> goalClassesToRemove.contains(goal.getClass()))
-				.forEach((goal) -> entity.goalSelector.removeGoal(goal));
 	}
 
 	public void setupSpiderGoals(SpiderEntity entity) {
-
+		// remove unwanted goals
+		final List<Class<? extends Goal>> goalClassesToRemove = Arrays.asList(LookRandomlyGoal.class, SpiderEntity.AttackGoal.class);
+		removeGoalsFromList(entity, goalClassesToRemove);
+		
+		entity.goalSelector.addGoal(0, new MoveTowardsNexusGoal(entity, Baseraids.baseraidsData.raidManager));
+		entity.goalSelector.addGoal(0, new DestroyNexusGoal(entity, Baseraids.baseraidsData.raidManager));
+		entity.goalSelector.addGoal(2, new SpiderEntity.AttackGoal(entity));		
 	}
 
 	public void setupSkeletonGoals(SkeletonEntity entity) {
 
+	}
+
+	public void setupPhantomGoals(PhantomEntity entity) {
+
+	}
+	
+	/**
+	 * Removes the specified AI goals from the given entity.
+	 * 
+	 * @param entity              the entity from which to remove the goals
+	 * @param goalClassesToRemove a list of classes of the types of goals to remove
+	 */
+	private void removeGoalsFromList(MonsterEntity entity, final List<Class<? extends Goal>> goalClassesToRemove) {
+		entity.goalSelector.getRunningGoals().filter((goal) -> goalClassesToRemove.contains(goal.getClass()))
+				.forEach((goal) -> entity.goalSelector.removeGoal(goal));
 	}
 }
