581c3c880429f93bacb11a8311dc5072a02bb1df
tatschi <natascha.may@outlook.de>
2022-06-30 15:26:48 +0200

    Restructured the commands

--- eclipse_project/src/main/java/may/baseraids/commands/BaseraidsCommands.java
+++ eclipse_project/src/main/java/may/baseraids/commands/BaseraidsCommands.java
@@ -7,11 +7,11 @@ import may.baseraids.NexusBlock;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraft.entity.player.ServerPlayerEntity;
 
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.FORGE)
 public class BaseraidsCommands {
@@ -27,28 +27,24 @@ public class BaseraidsCommands {
 	      dispatcher.register(
 	    		Commands.literal("baseraids")
 	    		.requires((commandSource) -> { return commandSource.hasPermissionLevel(2);})
-	    		.then(Commands.literal("raid")
-	    				.then(Commands.literal("start")
-	    						.executes((commandSource) -> {return startRaid(commandSource.getSource());}))
-	    				.then(Commands.literal("win")
-	    						.executes((commandSource) -> {return winRaid(commandSource.getSource());}))
-						.then(Commands.literal("lose")
-	    						.executes((commandSource) -> {return loseRaid(commandSource.getSource());}))
-	    		)
-	      		.then(Commands.literal("get")
-	      				.then(Commands.literal("timeUntilRaid")
-	      						.executes((commandSource) -> {return getTimeUntilRaid(commandSource.getSource());}))
-	      				.then(Commands.literal("level")
-	      						.executes((commandSource) -> {return getRaidLevel(commandSource.getSource());}))
-	      		)
-	      		.then(Commands.literal("give")
-	      				.then(Commands.literal("nexus")
-	      						.then(Commands.argument("target", EntityArgument.players())
-	      								.executes((commandSource) -> {return giveNexusToPlayer(commandSource.getSource(), EntityArgument.getPlayer(commandSource, "target"));})))
-	    		 ));
+				.then(Commands.literal("start_raid")
+						.executes((commandSource) -> {return startRaid(commandSource.getSource());}))
+				.then(Commands.literal("win_raid")
+						.executes((commandSource) -> {return winRaid(commandSource.getSource());}))
+				.then(Commands.literal("lose_raid")
+						.executes((commandSource) -> {return loseRaid(commandSource.getSource());}))
+	    		
+	      		.then(Commands.literal("get_timeUntilRaid")
+	      				.executes((commandSource) -> {return getTimeUntilRaid(commandSource.getSource());}))
+	      		.then(Commands.literal("get_level")
+	      				.executes((commandSource) -> {return getRaidLevel(commandSource.getSource());}))
+	      		
+	      		.then(Commands.literal("give_nexus")
+	      				.then(Commands.argument("target", EntityArgument.players())
+	      						.executes((commandSource) -> {return giveNexusToPlayer(commandSource.getSource(), EntityArgument.getPlayer(commandSource, "target"));})))
+	    		 );
 	}
 
-	
 	private static int giveNexusToPlayer(CommandSource source, ServerPlayerEntity target) {
 		return NexusBlock.giveNexusToPlayer(target) ? 0 : 1;
 	}
