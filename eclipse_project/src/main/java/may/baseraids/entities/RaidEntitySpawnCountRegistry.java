0b3ff01444e28dd6fe51f14303deffd3b5c89f20
tatschi <natascha.may@outlook.de>
2022-11-03 15:38:17 +0100

    Fixed unintended swap of spawn counts of skeletons and spiders

--- eclipse_project/src/main/java/may/baseraids/entities/RaidEntitySpawnCountRegistry.java
+++ eclipse_project/src/main/java/may/baseraids/entities/RaidEntitySpawnCountRegistry.java
@@ -10,19 +10,23 @@ public class RaidEntitySpawnCountRegistry {
 
 	private static Map<EntityType<?>, int[]> entitySpawnCountRegistry = new HashMap<EntityType<?>, int[]>();
 	
-	private static final int[] SPAWN_COUNT_ZOMBIES = {10, 10, 9, 7, 5, 15, 20};
-	private static final int[] SPAWN_COUNT_SKELETONS = {0, 3, 3, 5, 6, 10, 15};
-	private static final int[] SPAWN_COUNT_SPIDERS = {0, 0, 2, 4, 5, 8, 10};
-	private static final int[] SPAWN_COUNT_PHANTOMS = {0, 0, 0, 0, 0, 0, 0};
-	private static final int[] SPAWN_COUNT_ZOMBIFIED_PIGLINS = {0, 0, 1, 5, 10, 10, 10};
 	
+	private static final int[] SPAWN_COUNT_ZOMBIES = 			{10, 10, 9, 7, 5, 15, 20, 20};
+	private static final int[] SPAWN_COUNT_SKELETONS = 			{0, 3, 3, 5, 6, 10, 15, 15};
+	private static final int[] SPAWN_COUNT_SPIDERS = 			{0, 0, 2, 4, 5, 8, 10, 10};
+	private static final int[] SPAWN_COUNT_PHANTOMS = 			{0, 0, 0, 0, 0, 0, 0, 0};
+	private static final int[] SPAWN_COUNT_ZOMBIFIED_PIGLINS = 	{0, 0, 1, 5, 10, 10, 10, 10};
+	private static final int[] SPAWN_COUNT_WITHER_SKELETONS = 	{0, 0, 0, 0, 0, 0, 0, 1};
+	private static final int[] SPAWN_COUNT_CAVE_SPIDERS = 		{0, 0, 0, 0, 0, 1, 1, 1};
 	
 	public static void registerSpawnCounts() {
 		entitySpawnCountRegistry.put(EntityType.ZOMBIE, SPAWN_COUNT_ZOMBIES);
-		entitySpawnCountRegistry.put(EntityType.SPIDER, SPAWN_COUNT_SKELETONS);
-		entitySpawnCountRegistry.put(EntityType.SKELETON, SPAWN_COUNT_SPIDERS);
+		entitySpawnCountRegistry.put(EntityType.SKELETON, SPAWN_COUNT_SKELETONS);
+		entitySpawnCountRegistry.put(EntityType.SPIDER, SPAWN_COUNT_SPIDERS);
 		entitySpawnCountRegistry.put(EntityType.PHANTOM, SPAWN_COUNT_PHANTOMS);
 		entitySpawnCountRegistry.put(EntityType.ZOMBIFIED_PIGLIN, SPAWN_COUNT_ZOMBIFIED_PIGLINS);
+		entitySpawnCountRegistry.put(EntityType.WITHER_SKELETON, SPAWN_COUNT_WITHER_SKELETONS);
+		entitySpawnCountRegistry.put(EntityType.CAVE_SPIDER, SPAWN_COUNT_CAVE_SPIDERS);
 	}
 	
 	public static Set<EntityType<?>> getEntityTypesToSpawn(){
