d7ebf4da6c3cfd071de2cecc2a5267637368d811
tatschi <natascha.may@outlook.de>
2022-11-10 17:17:29 +0100

    Minor improvements in MoveTowardsNexusGoal
    https://github.com/tatschi/baseraids/issues/55

--- eclipse_project/src/main/java/may/baseraids/entities/ai/goal/MoveTowardsNexusGoal.java
+++ eclipse_project/src/main/java/may/baseraids/entities/ai/goal/MoveTowardsNexusGoal.java
@@ -8,7 +8,6 @@ import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.pathfinding.Path;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.server.ServerWorld;
 
 /**
  * This class defines the AI goal to move toward the nexus.
@@ -19,19 +18,16 @@ public class MoveTowardsNexusGoal extends Goal {
 
 	private MobEntity entity;
 	private RaidManager raidManager;
-	private BlockPos nexusPos;
 	private int distanceReached = 2;
 
 	public MoveTowardsNexusGoal(MobEntity entity, RaidManager raidManager) {
 		this.entity = entity;
 		this.raidManager = raidManager;
 		this.setMutexFlags(EnumSet.of(Goal.Flag.MOVE));
-		nexusPos = NexusBlock.getBlockPos();
 	}
 
 	public boolean shouldExecute() {
-		nexusPos = NexusBlock.getBlockPos();
-		
+		BlockPos nexusPos = NexusBlock.getBlockPos();		
 		
 		if(entity.getAttackTarget() != null || !raidManager.isRaidActive() || entity.getDistanceSq(nexusPos.getX(), nexusPos.getY(), nexusPos.getZ()) < distanceReached) {
 			return false;
@@ -41,29 +37,19 @@ public class MoveTowardsNexusGoal extends Goal {
 		return (path == null || !path.isFinished());
 	}
 
-	public boolean shouldContinueExecuting() {
-		nexusPos = NexusBlock.getBlockPos();
-		Path path = entity.getNavigator().getPath();
-		boolean shouldContinueExecuting = true;
-		shouldContinueExecuting &= raidManager.isRaidActive();
-		shouldContinueExecuting &= entity.getAttackTarget() == null;
-		shouldContinueExecuting &= entity.world instanceof ServerWorld;
-		shouldContinueExecuting &= entity.getDistanceSq(nexusPos.getX(), nexusPos.getY(), nexusPos.getZ()) >= distanceReached;
-		shouldContinueExecuting &= (path == null || !path.isFinished());
-		
-		return shouldContinueExecuting;
+	public boolean shouldContinueExecuting() {		
+		return shouldExecute();
 	}
 
 	public void startExecuting() {
-		nexusPos = NexusBlock.getBlockPos();
 	}
 
 	public void tick() {
+		BlockPos nexusPos = NexusBlock.getBlockPos();
 		entity.getNavigator().tryMoveToXYZ(nexusPos.getX(), nexusPos.getY(), nexusPos.getZ(), 1);
 	}
 
 	public void resetTask() {
-		entity.getLookController().setLookPosition(nexusPos.getX(), nexusPos.getY(), nexusPos.getZ());
 	}
 
 }
